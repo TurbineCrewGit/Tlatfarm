e49db6281fca51acb527d932909b9f691d635a1c
JenousLee <wnstj412@naver.com>
2025-02-07 08:36:15 +0900

    grid 수정

--- ds/src/3_Smartdronepage.js
+++ ds/src/3_Smartdronepage.js
@@ -3,6 +3,7 @@ import axios from "axios"; // Axios를 사용하여 백엔드와 통신
 import { Link } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Header from "./Components/Header.js";
+import MenuBar from "./MenuBar.js";
 import ThemeToggle from "./Components/ThemeToggle.js"; // 작은 따옴표 -> 큰 따옴표로 수정
 import "./Styles/App.css";
 import SmartdroneLists from "./3-1_SmartdroneLists.js";
@@ -17,9 +18,10 @@ function Smartdrone() {
   // return 시작
   return (
     <ThemeProvider theme={theme}>
-      <div className="planner">
+
         <Header />
-        <main className="drone-main">
+        <MenuBar />
+        <main className="drone-main main">
           <div className="drone-contents">
             {/* 드론 목록 테이블 */}
             <SmartdroneLists onDroneSelect={setSelectedDroneId}/>
@@ -30,7 +32,7 @@ function Smartdrone() {
           </div>
           {/* <p>임시) 선택된 드론 ID: {selectedDroneId}</p> */}
         </main>
-      </div>
+
     </ThemeProvider>
   );
 }
