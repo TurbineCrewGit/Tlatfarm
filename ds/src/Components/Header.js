78fa72cc923af846ff8d345955857a4f89a00333
JenousLee <wnstj412@naver.com>
2025-02-07 10:12:31 +0900

    grid (header) 수정

--- ds/src/Components/Header.js
+++ ds/src/Components/Header.js
@@ -7,9 +7,7 @@ import MenuBar from '../MenuBar';
 function Header() {
   return (
     <header className="header">
-      <Link to='/'style={{width:'350px', height:'150px'}} >
-        <img src={logo} alt="Turbine Planner" style={{width:'100%', height:'100%'}} />
-      </Link>
+
     </header>
   );
 }
